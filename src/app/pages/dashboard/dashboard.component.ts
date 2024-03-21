ea94bf63c539859c536050a5515a06ae66f03444
James Kokou GAGLO <freemanpolys@gmail.com>
2024-03-21 19:04:18 +0100

    feat: show email validation alert

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModalConfig, ModalComponent } from '../../_metronic/partials';
+import Swal, {SweetAlertOptions} from 'sweetalert2';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
   modalConfig: ModalConfig = {
     modalTitle: 'Modal title',
     dismissButtonLabel: 'Submit',
@@ -14,6 +15,27 @@ export class DashboardComponent {
   };
   @ViewChild('modal') private modalComponent: ModalComponent;
   constructor() {}
+  ngOnInit(): void {
+    const fireUser = JSON.parse(localStorage.getItem('fire_user')!);
+    const isEmailValNotif =  localStorage.getItem('email_validation_notif')
+    if(!fireUser.emailVerified && isEmailValNotif != 'true') {
+      Swal.fire({
+        title: 'Email validation',
+        text: 'Thank you for registering! To complete the registration process, please check your email inbox for a verification link. Click on the link provided to verify your email address and activate your account.',
+        icon: 'warning',
+        showCancelButton: false,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Close'
+      } as SweetAlertOptions).then((result) => {
+        if (result.value) {
+          //Do something here
+        }
+      }).finally(() => {
+        localStorage.setItem('email_validation_notif','true')
+      });
+  }
+  }
 
   async openModal() {
     return await this.modalComponent.open();
